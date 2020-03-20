4e1bc6df5af5a4c1cd8d7064433bee33c86bff58
GabrielRalph <gltralph@gmail.com>
2020-03-21 01:21:46 +1100

    comit

---
+++ main.js
@@ -0,0 +1,7 @@
+var list = ['AMME2500', 'COMP2123', 'FASHION', 'VISUALS'];
+var html = ''
+
+for(var i in list){
+  html += '<h1 onclick = "window.open(\'https://'+list[i].toLowerCase()+'.galetora.com\')">'+list[i]+'</h1>\n'
+}
+document.getElementById('app').innerHTML = html
