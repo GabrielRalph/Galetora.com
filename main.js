29178108a03873c213d136fb33885f48180a66be
GabrielRalph <gltralph@gmail.com>
2020-05-04 17:06:02 +1000

    wave visual

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ var html = ''
 for(var i in list){
   html += '<h1 onclick = "window.open(\'https://'+list[i].toLowerCase()+'.galetora.com\')">'+list[i]+'</h1>\n'
 }
-document.getElementById('app').innerHTML = html
+document.getElementById('app').innerHTML += html
